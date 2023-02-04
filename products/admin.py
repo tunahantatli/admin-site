b82ce262e8c10775b9f6dbef8b74935334e2c66c
Tunahan Tatli <tunahantatl@gmail.com>
2023-02-04 13:42:27 +0300

    products-edit-admin-site

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Product
 
 # Register your models here.
+admin.site.register(Product)
+
+admin.site.site_title = "tunahantatli.co"
+admin.site.site_header = "Admin Portal"
+admin.site.index_title = "Welcome to tunahantatli.co Admin Portal"
